8002de02633c5c8b85123394cb5e4a93e86a6c45
Guilherme Reis <guilhermereis2009@hotmail.com>
2025-03-25 22:50:22 -0300

    :lipstick: feat: adiciona screenshots das telas, ícones, imagens e toda a configuração de temas.

---
+++ app/src/main/java/com/guilhermereisapps/rickandmortykotlin/presentation/ui/theme/Color.kt
@@ -0,0 +1,24 @@
+package com.guilhermereisapps.rickandmortykotlin.presentation.ui.theme
+
+import androidx.compose.ui.graphics.Color
+
+val GreenPrimary = Color(0xFF97CE4C)
+val DarkGreenPrimary = Color(0xFF5A8A20)
+val SecondaryBlue = Color(0xFF46B2E0)
+val BackgroundDark = Color(0xFF121212)
+val BackgroundLight = Color(0xFFFFFFFF)
+val SurfaceDark = Color(0xFF1E1E1E)
+val SurfaceLight = Color(0xFFF2F2F2)
+val TextLight = Color(0xFF1A1A1A)
+val TextDark = Color(0xFFF0F0F0)
+
+// Cores para Tema de alto contraste
+val PrimaryHighContrast = Color(0xFF1D3557)
+val PrimaryVariantHighContrast = Color(0xFF457B9D)
+val SecondaryHighContrast = Color(0xFFE63946)
+val BackgroundHighContrast = Color(0xFFF1FAEE)
+val SurfaceHighContrast = Color(0xFFA8DADC)
+val OnPrimaryHighContrast = Color(0xFFFFFFFF)
+val OnSecondaryHighContrast = Color(0xFFFFFFFF)
+val OnBackgroundHighContrast = Color(0xFF1D3557)
+val OnSurfaceHighContrast = Color(0xFF1D3557)
